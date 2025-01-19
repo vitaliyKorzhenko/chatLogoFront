9a3922ecc93275e0e6115ff1ee247f2873e7bade
Vitaliy <vitaliykorzenkoua@gmail.com>
2025-01-19 19:49:35 +0200

    use domen
    use domen

--- src/apiConfig.ts
+++ src/apiConfig.ts
@@ -1,3 +1,3 @@
 let isProduction = true;
 export const apiUrl = isProduction ? 'http://167.172.179.104:4030/api/' : 'http://127.0.0.1:4030/api/';
-export const ioServer = isProduction ? 'https://167.172.179.104:4030' : 'http://127.0.0.1:4030';
+export const ioServer = isProduction ? 'https://chatgovorika.chat:4030' : 'http://127.0.0.1:4030';
