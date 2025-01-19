9da98c3d1d894759456167c5634eb98b87cbf388
Vitaliy <vitaliykorzenkoua@gmail.com>
2025-01-19 19:46:40 +0200

    https
    https

--- src/apiConfig.ts
+++ src/apiConfig.ts
@@ -1,3 +1,3 @@
 let isProduction = true;
-export const apiUrl = isProduction ? 'https://167.172.179.104:4030/api/' : 'http://127.0.0.1:4030/api/';
-export const ioServer = isProduction ? 'http://167.172.179.104:4030' : 'http://127.0.0.1:4030';
+export const apiUrl = isProduction ? 'http://167.172.179.104:4030/api/' : 'http://127.0.0.1:4030/api/';
+export const ioServer = isProduction ? 'https://167.172.179.104:4030' : 'http://127.0.0.1:4030';
